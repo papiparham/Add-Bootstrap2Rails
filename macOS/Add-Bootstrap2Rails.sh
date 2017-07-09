fdbc31552fbe28ddb28fbf3d42a37f97fd719c82
BPParham <bobby.parham@live.com>
2017-07-08 21:24:02 -0500

    Update Mac Minor Version 1

--- macOS/Add-Bootstrap2Rails.sh
+++ macOS/Add-Bootstrap2Rails.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Current_Version=1.0.0046
+Current_Version=1.2.0049
 Year=$(date +"%Y")
 
 home() {
@@ -96,7 +96,7 @@ EOT
 bundle install
   # "~/GITRepositories/$DOMAIN/$ENTITY/$PROJECT/trunk/$APPLICATION"
   echo "Importing style assets..."
-mv app/assets/stylesheets/application.css application.css.sass
+mv app/assets/stylesheets/application.css app/assets/stylesheets/application.css.sass
 sleep 10
 cat <<EOT >> $APPDIR/app/assets/stylesheets/application.css.sass
 @import "bootstrap-sprockets"
@@ -108,12 +108,17 @@ EOT
 # //= require bootstrap-sprockets
 # //= require_tree .
 #EOT
-  IMPORT='//= require bootstrap-sprockets\n//= require_tree .'
-find . -type f -name '$APPDIR/assets/javascripts/application.js' -exec sed -i '' s/'//= require_tree .'/$IMPORT/ {} +
+  SEARCH='//= require_tree .'
+  REPLACE='//= require bootstrap-sprockets\n//= require_tree .'
+# find . -type f -name '$APPDIR/assets/javascripts/application.js' -exec sed -i '' s/'//= require_tree .'/$IMPORT/ {} +
+export LC_CTYPE=C LANG=C
+find . -name '$APPDIR/assets/javascripts/application.js' -print0 | xargs -0 sed -i -e 's/$SEARCH/$REPLACE/g'
   # "~/GITRepositories/$DOMAIN/$ENTITY/$PROJECT/trunk/$APPLICATION"
   echo "Modifying layout..."
-  LAYOUT='    <div id="main-container" class="container">\n      <div class="row">\n        <div class="col-xs-3">\n          <h3>Tealeaf Academy Todo</h3>\n        </div>\n        <div class="col-xs-9">\n          <%= yield %>\n        </div>\n      </div>\n    </div>'
-find . -type f -name '$APPDIR/app/views/layouts/application.html.erb' -exec sed -i '' s/'<%= yield %>'/$LAYOUT/ {} +
+  SEARCH='<%= yield %>'
+  REPLACE='    <div id="main-container" class="container">\n      <div class="row">\n        <div class="col-xs-3">\n          <h3>Tealeaf Academy Todo</h3>\n        </div>\n        <div class="col-xs-9">\n          <%= yield %>\n        </div>\n      </div>\n    </div>'
+# find . -type f -name '$APPDIR/app/views/layouts/application.html.erb' -exec sed -i '' s/'<%= yield %>'/$LAYOUT/ {} +
+find . -name '$APPDIR/app/views/layouts/application.html.erb' -print0 | xargs -0 sed -i -e 's/$SEARCH/$REPLACE/g'
   echo "Done!..."
 }
 
